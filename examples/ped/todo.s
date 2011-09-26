13a1c5326d7be3fcaf1b9f3f2aabfc5abfaa4f78
Leonardo Cecchi <leonardoce@interfree.it>
2011-09-26 08:47:56 +0200

    fix todo

--- examples/ped/todo.s
+++ examples/ped/todo.s
@@ -1,6 +1,5 @@
 @ListItem { Comandi "p" e "z" non in history }
 @ListItem { Edit di una linea "le" }
 @ListItem { Gestire interrupt Ctrl-C }
-@ListItem { grep su files }
-@ListItem { sostituzione su files }
 @ListItem { Se molla una eccezione in salvataggio resetta comunque il flag modificato }
+@ListItem { Controllare in comando "w" che non si generino buffer con lo stesso nome }
