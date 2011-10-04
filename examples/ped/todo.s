36f567632302564afcf334b7fb63c0a6e67758cc
Leonardo Cecchi <leonardoce@interfree.it>
2011-10-04 09:04:07 +0200

    update todo list

--- examples/ped/todo.s
+++ examples/ped/todo.s
@@ -1,3 +1,4 @@
 @ListItem { Comandi "p" e "z" non in history }
 @ListItem { Correggere errore su ricerca con "?" }
 @ListItem { Ritorni carrelli in formato unix e dos }
+@ListItem { Correzione gestione degli errori sul comando "h" }
