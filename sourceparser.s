0b4e5350d618d6f0e0b0acb2e393d6fbd6cb4616
Leonardo Cecchi <leonardoce@interfree.it>
2012-04-16 15:40:05 +0200

    working on reverse

--- sourceparser.s
+++ sourceparser.s
@@ -21,6 +21,7 @@ This is an example of source code who can be read by Slit: @PP
 // [open] main
 int main(int argc, char **argv) {
   printf("Hello world!");
+  return 0;
 }
 // [close] main
 @End @Verbatim @PP @PP
@@ -96,11 +97,13 @@ begin
     begin
       line := MidStr (line, 7, Length(line));
       MacroName := Trim(line);
+      MarkerType := OpeningMarker;
     end
     else if AnsiStartsStr('[close]', line) then
     begin
       line := MidStr (line, 8, Length(line));
       MacroName := Trim(line);
+      MarkerType := ClosingMarker;
     end;
   end;
 end;
@@ -110,20 +113,147 @@ end;
 @Section @Title { Reading macros } 
 @Begin @PP
 
+Source files are read line by line:
+
 @d TSourceStream.ProcessFile
 @{
 procedure TSourceStream.ProcessFile;
 var
   currentLine : String;
+  currentMacroRecord : TMacroRecord;
+
+  macroNameStore: array of String;
+  macroNameStoreLength: integer;
+
+  tempMarkerName:String;
+  tempMarkerType:EMarkerType;
+
+  @<TSourceStream.ProcessFile, PushMacroName@>
+  @<TSourceStream.ProcessFile, PopMacroName@>
+  @<TSourceStream.ProcessFile, TopMacroName@>
 begin
+  macroNameStoreLength := 0;
+  CurrentMacroRecord := Nil;
+  SetLength (macroNameStore, 50);
+
   while not Eof() do
   begin
     currentLine := NextLine;
     if RowIsMarker (currentLine) then
     begin
+      InfoFromMarker (currentLine, tempMarkerName, tempMarkerType);
+      if tempMarkerType=OpeningMarker then
+      begin
+        @<TSourceStream.ProcessFile, opening marker@>
+      end
+      else
+      begin
+        @<TSourceStream.ProcessFile, closing marker@>
+      end;
       writeln (currentLine);
+    end
+    else
+    begin
+      @<TSourceStream.ProcessFile, code line@>
     end;
   end;
+end;
+@}
+
+We need a stack to manage the macro names. The stack is realized by
+the following functions: @PP
+
+@d TSourceStream.ProcessFile, PushMacroName
+@{
+procedure PushMacroName (macroName:String);
+begin
+  macroNameStore[macroNameStoreLength]:=macroName;
+  macroNameStoreLength := macroNameStoreLength + 1;
+  if macroNameStoreLength=50 then
+  begin
+    LogErrorMessage ('Maximum macro deep reached');
+    Abort();
+  end;
+end;
+@}
+
+@d TSourceStream.ProcessFile, PopMacroName
+@{
+procedure PopMacroName;
+begin
+  if macroNameStoreLength>0 then
+  begin
+    macroNameStoreLength := macroNameStoreLength - 1;
+  end
+end;
+@}
+
+@d TSourceStream.ProcessFile, TopMacroName
+@{
+function TopMacroName():String;
+begin
+  if macroNameStoreLength>0 then
+  begin
+    Result := macroNameStore[macroNameStoreLength-1];
+  end
+  else
+  begin
+    Result := '';
+  end;
+end;
+@}
+
+When a new macro is encountered the name is put on a stack:
+
+@d TSourceStream.ProcessFile, opening marker
+@{
+if CurrentMacroRecord<>Nil then
+begin
+  CurrentMacroRecord.AddContent ('@<' + tempMarkerName + '@>', FFileName, FCurrentLine);
+end;
+PushMacroName(tempMarkerName);
+CurrentMacroRecord := FMacroStore.GetMacro (tempMarkerName);
+@}
+
+When the macro is closed we will check is the name of the closed macro
+is synchronized with our stack:
+
+@d TSourceStream.ProcessFile, closing marker
+@{
+if CurrentMacroRecord<>Nil then
+begin
+  if tempMarkerName <> TopMacroName then
+  begin
+    LogErrorMessage (FFileName + ':' + IntToStr(FCurrentLine) + 
+      ' macro name error: should be ' + tempMarkerName);
+  end
+  else
+  begin
+    PopMacroName;
+    if macroNameStoreLength>0 then
+    begin
+      CurrentMacroRecord := FMacroStore.GetMacro(TopMacroName);
+    end
+    else
+    begin
+      CurrentMacroRecord := Nil;
+    end;
+  end;
+end
+else
+begin
+  LogErrorMessage (FFileName + ':' + IntToStr(FCurrentLine) + 
+    ' misplaced closing marker');
+end;
+@}
+
+When we encounder a code line we must insert it in the macro store:
+
+@d TSourceStream.ProcessFile, code line
+@{
+if CurrentMacroRecord<>Nil then
+begin
+  CurrentMacroRecord.AddContent (tempMarkerName, FFileName, FCurrentLine);
 end;
 @}
 
